74650014e0d23d90b4d195e4196d8c67f9b32f15
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-26 17:43:58 -0400

    Implement method to viewChild in list-children.component.ts

--- src/app/dashboard/child/list-children/list-children.component.ts
+++ src/app/dashboard/child/list-children/list-children.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChildService } from '../child.service';
 import { RequestChild } from '../models/request.child.model';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-list-children',
   templateUrl: './list-children.component.html',
@@ -10,14 +11,16 @@ export class ListChildrenComponent implements OnInit {
 
   children: RequestChild[] = [];
 
-  constructor(private childService: ChildService) { }
+  constructor(private childService: ChildService, private router: Router) { }
 
   ngOnInit(): void {
     this.childService.getChildren().subscribe(
       data => this.children = data
     );
   }
-
+  viewChild(id: number): void {
+    this.router.navigate(['/view-child', id]);
+  }
   calculateAge(dateOfBirth: Date): number {
     const birthDate = new Date(dateOfBirth);
     const today = new Date();
@@ -27,8 +30,6 @@ export class ListChildrenComponent implements OnInit {
     if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
       age--;
     }
-
     return age;
   }
-
 }
