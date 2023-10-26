e7294fbf606554f922ec0a1a0b5de1f9e87fc494
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-26 00:38:13 -0400

    Include vaccine route to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RegisterComponent } from './authentication/register/register.component'
 import { UserDashboardComponent} from "./dashboard/user-dashboard/user-dashboard.component";
 import { ChildListComponent } from './dashboard/child-list/child-list.component';
 import { AddChildComponent} from "./dashboard/add-child/add-child.component";
+import { VaccineListComponent } from './dashboard/vaccine-list/vaccine-list.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'dashboard', component: UserDashboardComponent},
   { path: 'child-list', component: ChildListComponent},
   { path: 'add-child', component: AddChildComponent},
+  { path: 'vaccine-list', component: VaccineListComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,19 @@ import {FormsModule} from "@angular/forms";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RegisterComponent } from './authentication/register/register.component';
 import { UserDashboardComponent } from './dashboard/user-dashboard/user-dashboard.component';
+import { ChildListComponent } from './dashboard/child-list/child-list.component';
+import { VaccineListComponent } from './dashboard/vaccine-list/vaccine-list.component';
+import { AddChildComponent } from './dashboard/add-child/add-child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    UserDashboardComponent
+    UserDashboardComponent,
+    ChildListComponent,
+    VaccineListComponent,
+    AddChildComponent
   ],
     imports: [
         BrowserModule,
