150ebff34024ba397592cd6495c08042bfdac70c
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-26 17:47:24 -0400

    Implement view-child.component.ts

--- src/app/dashboard/child/view-child/view-child.component.ts
+++ src/app/dashboard/child/view-child/view-child.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from "@angular/router";
+import { ChildService } from '../child.service';
+import {RequestChild} from "../models/request.child.model";
 
 @Component({
   selector: 'app-view-child',
   templateUrl: './view-child.component.html',
   styleUrls: ['./view-child.component.css']
 })
-export class ViewChildComponent {
+export class ViewChildComponent implements OnInit {
 
+  child!: RequestChild;
+
+  constructor(private route: ActivatedRoute, private childService: ChildService) { }
+
+  ngOnInit(): void {
+    const idString = this.route.snapshot.paramMap.get('id');
+    if (idString !== null) {
+      const id = Number(idString);  // Convert string to number using Number() constructor
+      this.childService.getChildById(id).subscribe(
+        data => this.child = data
+      );
+    }
+  }
+
+  calculateAge(dateOfBirth: Date): number {
+    const birthDate = new Date(dateOfBirth);
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
 }
