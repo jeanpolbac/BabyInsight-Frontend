2e5182b0c08d96597f575b2007d676ad62cbe160
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-26 11:00:15 -0400

    Create initial list-children component

---
+++ src/app/dashboard/child/list-children/list-children.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-children',
+  templateUrl: './list-children.component.html',
+  styleUrls: ['./list-children.component.css']
+})
+export class ListChildrenComponent {
+
+}
