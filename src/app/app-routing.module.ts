a73420e284537fef41520dc8b7631aa94274fd4c
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-26 00:32:34 -0400

    Add child list route to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
 import { RegisterComponent } from './authentication/register/register.component';
 import { UserDashboardComponent} from "./dashboard/user-dashboard/user-dashboard.component";
+import { ChildListComponent } from './dashboard/child-list/child-list.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent},
   { path: 'dashboard', component: UserDashboardComponent},
+  { path: 'child-list', component: ChildListComponent},
 ];
 
 @NgModule({
