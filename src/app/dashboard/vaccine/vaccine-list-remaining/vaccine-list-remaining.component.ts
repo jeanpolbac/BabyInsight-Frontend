899f3da7a71170ca482a26ae401bea956c12726f
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-26 11:02:03 -0400

    Create initial vaccine-list-remaining component

---
+++ src/app/dashboard/vaccine/vaccine-list-remaining/vaccine-list-remaining.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vaccine-list-remaining',
+  templateUrl: './vaccine-list-remaining.component.html',
+  styleUrls: ['./vaccine-list-remaining.component.css']
+})
+export class VaccineListRemainingComponent {
+
+}
