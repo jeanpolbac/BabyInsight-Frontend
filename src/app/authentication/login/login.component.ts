c80e5a0fed372a7ea8991483728e733c12351509
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-25 22:16:42 -0400

    Refactor login method in login.component.ts to use onSubmit use on the form

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private authenticationService: AuthenticationService) {
+  }
+
+  onSubmit() {
+    if (this.email && this.password) {
+      this.authenticationService.login(this.email, this.password).subscribe({
+        next: (response) => {
+          console.log('Login successful', response);
+          // Load dashboard if successful
+        },
+        error: (error) => {
+          console.log('Login failed', error);
+          // show an error message to the user
+        }
+      });
+    } else {
+      console.log('Email and password are required');
+      // show error message to the user here
+    }
+  }
 }
+
+
+
+
+
