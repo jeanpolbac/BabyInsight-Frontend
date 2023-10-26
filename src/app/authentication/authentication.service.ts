4517e1cd282f5d47ebb5269bbb4b497ce3c890b3
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-25 22:44:53 -0400

    Fix login method in authentication.service.ts to create HttpHeaders to successfully connect with backend

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import { throwError } from 'rxjs/internal/observable/throwError';
 import {catchError, Observable} from "rxjs";
 
@@ -23,15 +23,40 @@ export class AuthenticationService {
    */
   login(email: string, password: string): Observable<any> {
     // Create an object containing the user's email and password
-    const authInfo = {email, password};
+    const authInfo = {emailAddress: email, password: password};
 
+    // Set the headers for the HTTP request
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json' // Set the Content-Type header to JSON
+    });
     // Make an HTTP POST request to the login endpoint
-    return this.http.post(`${this.baseUrl}/auth/users/login/`, authInfo)
+    return this.http.post(`${this.baseUrl}/auth/users/login/`, authInfo, { headers })
       .pipe(
         catchError(error => {
           // Log and rethrow any errors that occur during login
           console.log(`Login failed: ${error.status}`);
-          return throwError(error);
+          return throwError(() => error);
+        })
+      );
+  }
+
+
+  /**
+   * Register a new user
+   *
+   * @param email - The user's email address
+   * @param password - The user's password
+   * @returns An Observable with the registration result or an error
+   */
+  register(email: string, password: string): Observable<any> {
+    const authInfo = { email, password };
+    return this.http.post(`${this.baseUrl}/auth/users/register/`, authInfo)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          // Log the error
+          console.error('Registration failed:', error);
+          // Return an observable with the error
+          return throwError(() => error);
         })
       );
   }
