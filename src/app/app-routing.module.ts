fa2800e6ca761439b73df359a5d940355c8fe168
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-26 00:15:52 -0400

    Add user dashboard route to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
 import { RegisterComponent } from './authentication/register/register.component';
+import { UserDashboardComponent} from "./dashboard/user-dashboard/user-dashboard.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent},
+  { path: 'dashboard', component: UserDashboardComponent},
 ];
 
 @NgModule({
