bc5609d8d6e5d447244d7f4034a05dacdf3a8ffa
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-25 21:58:58 -0400

    Add HttpClientModule to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './authentication/login/login.component';
+import {FormsModule} from "@angular/forms";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterComponent } from './authentication/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        NgbModule,
+        HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
