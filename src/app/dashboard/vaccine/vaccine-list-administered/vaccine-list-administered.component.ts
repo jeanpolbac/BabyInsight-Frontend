8ac63c79cba206947c06c43b12568e150e82bc9e
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-27 03:40:55 -0400

    Add CSS and doctring to vaccine-list-administered.component.html

--- src/app/dashboard/vaccine/vaccine-list-administered/vaccine-list-administered.component.ts
+++ src/app/dashboard/vaccine/vaccine-list-administered/vaccine-list-administered.component.ts
@@ -8,28 +8,41 @@ import { ActivatedRoute } from "@angular/router";
   templateUrl: './vaccine-list-administered.component.html',
   styleUrls: ['./vaccine-list-administered.component.css']
 })
+/**
+ * Component for displaying administered vaccines for a child.
+ */
 export class VaccineListAdministeredComponent implements OnInit {
   public administeredVaccines: Vaccine[] = [];
   public childId!: number;
 
+  /**
+   * Constructor for the VaccineListAdministeredComponent.
+   * @param vaccineService - Service for fetching vaccine data.
+   * @param route - ActivatedRoute for accessing route parameters.
+   */
+  constructor(private vaccineService: VaccineService, private route: ActivatedRoute) {}
 
-  constructor(private vaccineService: VaccineService, private route: ActivatedRoute) {
-  }
-
+  /**
+   * Initializes the component by fetching the child ID from route parameters
+   * and fetching the administered vaccines for the child.
+   */
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.childId = +params['id'];
-      this.fetchOverdueVaccines();
+      this.fetchAdministeredVaccines();
     });
   }
 
-  fetchOverdueVaccines(): void {
-    this.vaccineService.getOverdueVaccines(this.childId).subscribe(
+  /**
+   * Fetches the administered vaccines for the child with the given ID.
+   */
+  fetchAdministeredVaccines(): void {
+    this.vaccineService.getAdministeredVaccines(this.childId).subscribe(
       data => {
         this.administeredVaccines = data;
       },
       error => {
-        console.error('Error fetching overdue vaccines:', error);
+        console.error('Error fetching administered vaccines:', error);
       }
     );
   }
