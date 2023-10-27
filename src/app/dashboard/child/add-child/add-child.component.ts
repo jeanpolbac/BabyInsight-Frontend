683a35754ab80321774b97c5cc6b324aba215498
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-27 03:35:43 -0400

    Add docstring to add-child component

--- src/app/dashboard/child/add-child/add-child.component.ts
+++ src/app/dashboard/child/add-child/add-child.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { ChildService } from 'src/app/dashboard/child/child.service';
-import { Child} from "../models/child.model";
+import { Child } from "../models/child.model";
 
 @Component({
   selector: 'app-add-child',
   templateUrl: './add-child.component.html',
   styleUrls: ['./add-child.component.css']
 })
+/**
+ * Component for adding a new child.
+ */
 export class AddChildComponent {
-  child: Child = {name: '', dateOfBirth: ''};
+  child: Child = { name: '', dateOfBirth: '' };
 
   constructor(private childService: ChildService) {
   }
 
+  /**
+   * Handles the form submission to add a new child.
+   */
   onSubmit() {
     this.childService.addChild(this.child).subscribe(
       (response) => {
@@ -24,8 +30,3 @@ export class AddChildComponent {
     );
   }
 }
-
-
-
-
-
