b20c5e617abba01165032b8729121a18f391755a
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-27 03:40:15 -0400

    Add doctring to vaccine.model.ts

--- src/app/dashboard/vaccine/vaccine.model.ts
+++ src/app/dashboard/vaccine/vaccine.model.ts
@@ -1,6 +1,9 @@
+/**
+ * Represents a vaccine record.
+ */
 export interface Vaccine {
-  vaccineId: any;
-  name: string;
-  recommendedAge: number;
-  dateAdministered: string;
+  vaccineId: any;                // The unique identifier of the vaccine.
+  name: string;                 // The name of the vaccine.
+  recommendedAge: number;       // The recommended age for administering the vaccine.
+  dateAdministered: string;     // The date when the vaccine was administered (in string format).
 }
