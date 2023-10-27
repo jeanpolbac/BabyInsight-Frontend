208a3c8182599cb82137fd16bb77c910c73cbd03
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-26 20:02:22 -0400

    Implement vaccine-list-administered.component.ts

--- src/app/dashboard/vaccine/vaccine-list-administered/vaccine-list-administered.component.ts
+++ src/app/dashboard/vaccine/vaccine-list-administered/vaccine-list-administered.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Vaccine} from "../vaccine.model";
+import {VaccineService} from "../vaccine.service";
 
 @Component({
   selector: 'app-vaccine-list-administered',
   templateUrl: './vaccine-list-administered.component.html',
   styleUrls: ['./vaccine-list-administered.component.css']
 })
-export class VaccineListAdministeredComponent {
+export class VaccineListAdministeredComponent implements OnInit {
+  public administeredVaccines: Vaccine[] = [];
+  public childId: number = 1;
 
+
+  constructor(private vaccineService: VaccineService) {
+  }
+
+  ngOnInit(): void {
+    this.fetchAdministeredVaccines();
+  }
+
+  fetchAdministeredVaccines(): void {
+    this.vaccineService.getAdministeredVaccines(this.childId).subscribe(
+      (vaccines: Vaccine[]) => {
+        this.administeredVaccines = vaccines;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
