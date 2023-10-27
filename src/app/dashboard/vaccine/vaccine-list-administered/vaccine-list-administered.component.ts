5054e58e52605412f868d88a7738de76dfd56d3b
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-26 20:35:23 -0400

    Refactor vaccine-list-administered.component.ts to retrieve administered by childId dynamically

--- src/app/dashboard/vaccine/vaccine-list-administered/vaccine-list-administered.component.ts
+++ src/app/dashboard/vaccine/vaccine-list-administered/vaccine-list-administered.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Vaccine} from "../vaccine.model";
-import {VaccineService} from "../vaccine.service";
+import { Vaccine } from "../vaccine.model";
+import { VaccineService } from "../vaccine.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-vaccine-list-administered',
@@ -9,23 +10,26 @@ import {VaccineService} from "../vaccine.service";
 })
 export class VaccineListAdministeredComponent implements OnInit {
   public administeredVaccines: Vaccine[] = [];
-  public childId: number = 1;
+  public childId!: number;
 
 
-  constructor(private vaccineService: VaccineService) {
+  constructor(private vaccineService: VaccineService, private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    this.fetchAdministeredVaccines();
+    this.route.params.subscribe(params => {
+      this.childId = +params['id'];
+      this.fetchOverdueVaccines();
+    });
   }
 
-  fetchAdministeredVaccines(): void {
-    this.vaccineService.getAdministeredVaccines(this.childId).subscribe(
-      (vaccines: Vaccine[]) => {
-        this.administeredVaccines = vaccines;
+  fetchOverdueVaccines(): void {
+    this.vaccineService.getOverdueVaccines(this.childId).subscribe(
+      data => {
+        this.administeredVaccines = data;
       },
-      (error) => {
-        console.error(error);
+      error => {
+        console.error('Error fetching overdue vaccines:', error);
       }
     );
   }
