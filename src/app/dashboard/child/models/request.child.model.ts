1977acf995c2c5c7309b5cdb158f9786e1680ba2
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-27 03:39:38 -0400

    Add docstring to request.child.model.ts

--- src/app/dashboard/child/models/request.child.model.ts
+++ src/app/dashboard/child/models/request.child.model.ts
@@ -1,5 +1,8 @@
+/**
+ * Represents a request to create or update child information.
+ */
 export interface RequestChild {
-  id: any;
-  name: string;
-  dateOfBirth: Date;
+  id: any;                // The unique identifier of the child (can be any type).
+  name: string;           // The name of the child.
+  dateOfBirth: Date;     // The date of birth of the child.
 }
