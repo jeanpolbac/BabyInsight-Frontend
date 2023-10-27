f3fbd5cf0418d7271e57fb797de224854467569e
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-27 03:36:32 -0400

    Add docstring to authentication.service.ts

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -6,6 +6,9 @@ import { catchError, Observable, tap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * Service for handling user authentication and authorization.
+ */
 export class AuthenticationService {
 
   // Define the base URL for the backend API
@@ -20,48 +23,57 @@ export class AuthenticationService {
   }
 
   /**
-   * Perform user login
+   * Perform user login.
    *
-   * @param email - The user's email address
-   * @param password - The user's password
-   * @returns An Observable with the login result or an error
+   * @param email - The user's email address.
+   * @param password - The user's password.
+   * @returns An Observable with the login result or an error.
    */
   login(email: string, password: string): Observable<any> {
     const authInfo = { emailAddress: email, password: password };
     return this.http.post(`${this.baseUrl}/auth/users/login/`, authInfo).pipe(
-        tap((response: any) => {
-          if (response.jwt) {
-            localStorage.setItem('jwt', response.jwt);
-            this.userId = response.user; // Set the userId property
-          }
-        })
+      tap((response: any) => {
+        if (response.jwt) {
+          localStorage.setItem('jwt', response.jwt);
+          this.userId = response.user; // Set the userId property
+        }
+      })
     );
   }
 
   /**
-   * Register a new user
+   * Register a new user.
    *
-   * @param email - The user's email address
-   * @param password - The user's password
-   * @returns An Observable with the registration result or an error
+   * @param email - The user's email address.
+   * @param password - The user's password.
+   * @returns An Observable with the registration result or an error.
    */
   register(email: string, password: string): Observable<any> {
-    const authInfo = {emailAddress: email, password: password};
-    return this.http.post(`${this.baseUrl}/auth/users/register/`, authInfo)
-        .pipe(
-            catchError((error: HttpErrorResponse) => {
-              // Log the error
-              console.error('Registration failed:', error);
-              // Return an observable with the error
-              return throwError(() => error);
-            })
-        );
+    const authInfo = { emailAddress: email, password: password };
+    return this.http.post(`${this.baseUrl}/auth/users/register/`, authInfo).pipe(
+      catchError((error: HttpErrorResponse) => {
+        // Log the error
+        console.error('Registration failed:', error);
+        // Return an observable with the error
+        return throwError(() => error);
+      })
+    );
   }
 
+  /**
+   * Get the JWT token.
+   *
+   * @returns The JWT token or null if not available.
+   */
   getJwtToken(): string | null {
     return this.jwtToken;
   }
 
+  /**
+   * Get the user's ID.
+   *
+   * @returns The user's ID or null if not available.
+   */
   getUserId(): any | null {
     return this.userId;
   }
