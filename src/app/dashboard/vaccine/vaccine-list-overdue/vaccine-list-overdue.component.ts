0e22149abee2e25fef2e42ee5fe4a64c86913a50
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-26 20:33:44 -0400

    Implement vaccine-list-overdue.component.ts

--- src/app/dashboard/vaccine/vaccine-list-overdue/vaccine-list-overdue.component.ts
+++ src/app/dashboard/vaccine/vaccine-list-overdue/vaccine-list-overdue.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VaccineService} from "../vaccine.service";
+import { Vaccine } from '../vaccine.model';
 
 @Component({
   selector: 'app-vaccine-list-overdue',
   templateUrl: './vaccine-list-overdue.component.html',
   styleUrls: ['./vaccine-list-overdue.component.css']
 })
-export class VaccineListOverdueComponent {
+export class VaccineListOverdueComponent implements OnInit{
+  overdueVaccines: Vaccine[] = [];
+  childId: number = 1;
 
+  constructor(private vaccineService: VaccineService) {
+  }
+
+  ngOnInit(): void {
+    this.fetchOverdueVaccines();
+  }
+
+
+  fetchOverdueVaccines(): void {
+    this.vaccineService.getOverdueVaccines(this.childId).subscribe(
+      data => {
+        this.overdueVaccines = data;
+      },
+      error => {
+        console.error('Error fetching overdue vaccines:', error);
+      }
+    );
+  }
 }
