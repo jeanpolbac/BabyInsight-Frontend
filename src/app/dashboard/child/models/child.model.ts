26918b60fd0a82b5fade9e0a318e6c734cc18fa0
Jean-Pol Bacquet <jeanpolbac@gmail.com>
2023-10-27 03:37:01 -0400

    Add docstring to child.model.ts

--- src/app/dashboard/child/models/child.model.ts
+++ src/app/dashboard/child/models/child.model.ts
@@ -1,4 +1,7 @@
+/**
+ * Represents information about a child.
+ */
 export interface Child {
-  name: string;
-  dateOfBirth: string;
+  name: string;          // The name of the child.
+  dateOfBirth: string;  // The date of birth of the child (in string format).
 }
